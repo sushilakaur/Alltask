2866384fd1c7ddaf6af147e0e020f9fc76eced0e
maulikaluthra <mluthra_be19@thapar.edu>
2020-07-12 02:20:27 +0530

    navbar2

--- LANDING PAGE/script.js
+++ LANDING PAGE/script.js
@@ -1,7 +1,12 @@
+console.log("Hello! ");
+
 const nav = document.querySelector(".nav");
+
+console.log(nav);
 
 window.onscroll = function () {
   var top = window.scrollY;
+  console.log(top);
   if (top >= 145) {
     nav.classList.add("active");
   } else {
